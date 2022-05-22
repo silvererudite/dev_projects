e16adcc1789de3e1d25f8326a081d1e175910cee
silvererudite <shamima2hossain@gmail.com>
2022-05-22 22:27:23 +0600

    adding tic-tact-toe initial

---
+++ games/tic-tac-toe.py
@@ -0,0 +1,16 @@
+state = [[0,0,0],[0,0,0], [0,0,0]]
+move = 0
+def make_move(row, col, val):
+  if is_empty():
+    state[row, col] = val
+    move += 1
+
+def is_empty():
+  return 0 in state
+
+def winning_state():
+  if move == 3:
+    
+
+
+
